1ad027cfbad9df4a7d9135ff13fc801355fe88cf
unnithetech <165456763+unnithetech@users.noreply.github.com>
2024-04-01 15:55:56 +0000

    learning

--- learning.py
+++ learning.py
@@ -6,5 +6,66 @@
 # print(type(a))
 
 # print(70 + float("100.5"))
+# print(str(70)+ str(100))
 
-print(str(70)+ str(100))
+# calculation symbols in python
+#  +  is for addition                       print(7+5)
+#  -  is for subtraction                    print(7-5)
+# *   is for multiplication                 print(7*5) 
+# /   is for division                       print(7/5)
+# **  is for the exponential calculations   print(7**5)
+
+# BODMAS/PEMDAS 
+# () Brackets/Parentheses
+# ** Order of exponents/Exponents
+# /  Division
+# *  Multiplication
+# +  Addition
+# -  Subtraction
+
+# print(3*5-6+8/20**3)
+# print(3*(3+3)/3-3)
+# print(3*3+3/3-3)
+
+# print(round(2.6666666666665, 2))
+
+# print(8 / 3)
+
+# print(int(8 // 3))
+
+# score = 25
+# print("your score is " + str(score))
+
+
+# bill
+# thoothbrush is rupees 250 
+# thoothpaste is rupees 100
+# laptop is rupees 25000
+# i phone is rupees 12000
+
+# a = 250
+# b = 100
+# c = 25000
+# d =12000
+
+# print(a+b*c+d)
+
+
+
+
+
+print("welcome to the roller coster")
+hieght = int(input("what is your height in cm"))
+
+if hieght >= 120:
+    print("perfect!")
+else:print("Grow tall for a seat! ")
+age = int(input("what is your age"))
+
+if age < 12:
+        print("your total pay is $5")
+elif age <= 18:
+    print("your total pay would be $7")
+
+else:print("your total pay would be $12")    
+
